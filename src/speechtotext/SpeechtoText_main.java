fa60944183fe3ce729ddaff93758586d0766a4c2
1618037 <1618037@cs16037>
2018-10-05 15:57:52 +0900

    speechtotext

---
+++ src/speechtotext/SpeechtoText_main.java
@@ -0,0 +1,53 @@
+package speechtotext;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import com.ibm.watson.developer_cloud.speech_to_text.v1.SpeechToText;
+import com.ibm.watson.developer_cloud.speech_to_text.v1.model.RecognizeOptions;
+import com.ibm.watson.developer_cloud.speech_to_text.v1.model.SpeechRecognitionResults;
+import com.ibm.watson.developer_cloud.text_to_speech.v1.model.SynthesizeOptions;
+
+public class SpeechtoText_main {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			SpeechToText service = new SpeechToText();
+		    service.setUsernameAndPassword("99aca234-f422-4112-9a60-93f26a3dd22a", "4h44R1WPnKyr");
+
+		    File audio = new File("audio/katou.wav");
+		    RecognizeOptions options = null;
+				   try {
+					   options = new RecognizeOptions.Builder()
+						.model("ja-JP_BroadbandModel")
+					    .audio(audio)
+				        .contentType(RecognizeOptions.ContentType.AUDIO_WAV)
+				        .build();
+				   } catch (FileNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					   SpeechRecognitionResults transcript = service.recognize(options).execute();
+					   
+					   System.out.println(transcript);
+			}
+		private static void writeToFile(InputStream in, File file) {
+			   try {
+			     OutputStream out = new FileOutputStream(file);
+			     byte[] buf = new byte[1024];
+			     int len;
+			     while ((len = in.read(buf)) > 0) {
+			       out.write(buf, 0, len);
+			     }
+			     out.close();
+			     in.close();
+			   } catch (Exception e) {
+			     e.printStackTrace();
+			   }
+			 }
+
+	}
